7e1ac254bf51d17fcbbf083c9492a416b207744e
foshay <foshayi@oregonstate.edu>
2020-02-02 17:06:28 -0800

    upgrading and updating now

--- get_docker_mint.sh
+++ get_docker_mint.sh
@@ -7,4 +7,5 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
+sudo apt upgrade
 sudo apt install docker-ce docker-ce-cli containerd.io
