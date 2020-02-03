f10d0c811218c6eafe935bca7e994dd1504b6e6b
foshay <foshayi@oregonstate.edu>
2020-02-02 16:59:13 -0800

    fixing script

--- get_docker_mint.sh
+++ get_docker_mint.sh
@@ -5,8 +5,6 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   bionic \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
