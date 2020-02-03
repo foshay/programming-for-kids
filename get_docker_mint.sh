186643bc2d7fb0886cad715fe6df04c6059a79c4
foshay <foshayi@oregonstate.edu>
2020-02-02 17:04:43 -0800

    trying to get this to work

--- get_docker_mint.sh
+++ get_docker_mint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Installs docker on linux minut
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
