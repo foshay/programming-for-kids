95a0ce98c9f459825bbf55fa7447d1f50ee58f8c
Isak Foshay <foshayi@oregonstate.edu>
2020-02-08 14:17:16 -0800

    added some styling and comments. Also a button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import ReactBlocklyComponent from 'react-blockly';
 
 function App() {
   return (
     <div className="App">
       
-    <div style={{height: "600px", width: "800px"}} id="blockly">
-    </div>
-    <textarea id="code" style={{height: "200px", width: "400px"}} value=""></textarea>
-    
+    <div style={{height: "600px", width: "800px"}} id="blockly"></div>
+    <textarea disabled="yes" id="code" style={{height: "200px", width: "400px"}} value=""></textarea>
+    <button id="gradeButton">Grade code</button>
     </div>
     
   );
--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Blockly from 'blockly';
-import PythonGenerator from 'blockly/python';
+
 
 import ReactBlocklyComponent from 'react-blockly';
 import ConfigFiles from 'react-blockly/src/initContent/content';
 import parseWorkspaceXml from 'react-blockly/src/BlocklyHelper';
 
-class TestEditor extends React.Component {
+class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +15,9 @@ class TestEditor extends React.Component {
     };
   }
 
-  componentDidMount = () => {
+//this is optional for adding custom categories of blocks
+/*
+componentDidMount = () => {
     window.setTimeout(() => {
       this.setState({
         toolboxCategories: this.state.toolboxCategories.concat([
@@ -42,22 +44,32 @@ class TestEditor extends React.Component {
       });
     }, 2000);
   }
+*/
 
   workspaceDidChange = (workspace) => {
-    workspace.registerButtonCallback('sendToGrade', () => {
+      //this part you can do something when the workspace changes (when they place or move a block)
+    /*
+      workspace.registerButtonCallback('sendToGrade', () => {
       alert('Sent to grading script');
     });
+    */
     //We can use this for saving user's progress
-    //const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
-    //document.getElementById('generated-xml').innerText = newXml;
-
+    /*
+    const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+    document.getElementById('generated-xml').innerText = newXml;
+    */
+    //this prints out the blocks to actual python code to the page.
+    //TODO send the const code to backend for grading
+    require('blockly/python');
     const code = Blockly.Python.workspaceToCode(workspace);
     document.getElementById('code').value = code;
   }
 
   render = () => (
     <ReactBlocklyComponent.BlocklyEditor
+        // The block categories to be available.
       toolboxCategories={this.state.toolboxCategories}
+      //this is obvious what it does
       workspaceConfiguration={{
         grid: {
           spacing: 20,
@@ -66,14 +78,22 @@ class TestEditor extends React.Component {
           snap: true,
         },
       }}
+      //we can possibly change the initial xml on a per lesson basis... or not
       initialXml={ConfigFiles.INITIAL_XML}
+      //the div wrapper that will be generated for blockly
       wrapperDivClassName="fill-height"
+      //what method to call when the workspace changes
       workspaceDidChange={this.workspaceDidChange}
     />
   )
 }
 
+//on load we create an element of the class created above and insert into the element 'blockly'
 window.addEventListener('load', () => {
-  const editor = React.createElement(TestEditor);
-  ReactDOM.render(editor, document.getElementById('blockly'));
+    const button = document.getElementById('gradeButton');
+    button.addEventListener('click', function(e) {
+        alert(document.getElementById('code').value);
+    });
+    const editor = React.createElement(Editor);
+    ReactDOM.render(editor, document.getElementById('blockly'));
 });
