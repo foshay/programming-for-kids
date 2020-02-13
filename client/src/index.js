bcba180f6867c66e31f8d5e39bda4cb7bbb4f50e
foshay <foshayi@oregonstate.edu>
2020-02-12 19:52:35 -0800

    server comm up

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-require('./Editor.jsx.js');
+require('./Editor.jsx');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
