82be50e4369cc3c90f087f1be6e059343987c9c7
foshay <foshayi@oregonstate.edu>
2020-02-12 19:40:11 -0800

    server testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
+class App extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+  
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+  
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    
+    this.setState({ responseToPost: body });
+  };
+  
+render() {
+    return (
+      <div className="App">
     <div style={{height: "600px", width: "800px"}} id="blockly"></div>
     <textarea disabled="yes" id="code" style={{height: "200px", width: "400px"}} value=""></textarea>
     <button id="gradeButton">Grade code</button>
-    </div>
-    
-  );
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
