a412066591f7b5929e9016c0a0cf5fc3df4b9d31
foshay <foshayi@oregonstate.edu>
2020-02-15 13:52:11 -0800

    grading

---
+++ temp7688.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def grade():
+ results = usercode()
+ if (results == 20):
+  print("True")
+ else:
+  print("False")
+
+def usercode():
+# Describe this function...
+def do_something():
+  print(abc)
+  return 20
+
