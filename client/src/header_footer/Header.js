f7c56c29be8c7595a03a444dd15ee1391596b764
roy017 <41708735+roy017@users.noreply.github.com>
2020-02-23 19:22:33 -0800

    update header.js to put in a popover menu
    popover menu is in but looks bad

--- client/src/header_footer/Header.js
+++ client/src/header_footer/Header.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import { AnchorButton, Button, Code, H5, Intent, Switch as bpSwitch,Navbar, Alignment } from "@blueprintjs/core";
+import { AnchorButton, Button, Code, H5, Intent, Switch as bpSwitch,Navbar, Alignment, Menu, MenuDivider, MenuItem, Popover, Position, Classes } from "@blueprintjs/core";
 import HomeScreen from "../Menus/HomeScreen.js"
 import LessonMenu from "../Menus/LessonMenu.js"
 
 class Header extends Component {
     render(){
+        const popMenu = (
+            <Menu className={Classes.DARK}>
+                <h6>
+                    <ul class="bp3-menu bp3-elevation-1">
+                        <li><Link to="/Home"><MenuItem class="bp3-menu-item bp3-icon-home" icon="home" text="Home"/></Link></li>
+                        <Link to="/LessonMenu"><MenuItem class="bp3-menu-item bp3-icon-book"  icon="book" text="Lessons" /></Link>
+                        <MenuDivider />
+                        <MenuItem class="bp3-menu-item bp3-icon-cog" icon="cog" text="Settings">
+                            {/* TODO */}
+                            <MenuItem class="bp3-menu-item" text="option coming soon"/>
+                        </MenuItem>
+                        <MenuDivider />
+                        <Link to="/"><MenuItem class="bp3-menu-item bp3-icon-log-out" icon="log-out" text="Log-Out" /></Link>
+                    </ul>
+                </h6>
+            </Menu>
+        );
+
         return(
             <div>
-                {/* <Router> */}
-                    {/* <Route exact={true} path="/" component={HomeScreen} />
-                    <Route exact={true} path="/LessonMenu" component={LessonMenu} /> */}
-                    {/* <Link to="/"><button>Home</button></Link>
-                    <Link to="/LessonMenu"><button>Lessons</button></Link> */}
-
-                <Navbar>
+                {/* <Navbar>
                     <Navbar.Group align={Alignment.LEFT}>
                         <Navbar.Heading>Teach kids code</Navbar.Heading>
                         <Navbar.Divider />
-                        <Link to="/Home"><Button href="#/Home" className="bp3-minimal" icon="home" text="Home" /></Link>
+                        <Link to="/Home"><Button className="bp3-minimal" icon="home" text="Home" /></Link>
                         <Link to="/LessonMenu"><Button className="bp3-minimal" icon="book" text="Lessons" /></Link>
+                        <Link to="/"><Button className="bp3-minimal" icon="log-out" text="Log-Out" /></Link>
                     </Navbar.Group>
-                </Navbar>
-
-            {/* </Router> */}
+                </Navbar> */}
+                <Popover content={popMenu} position={Position.LEFT_TOP} className={Classes.DARK}>
+                    <Button icon="menu" />
+                </Popover>
         </div>
         )
     }
