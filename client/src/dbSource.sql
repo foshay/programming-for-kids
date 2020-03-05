e9ef0ba45802262ed456bacb5a89a28029727d61
foshay <foshayi@oregonstate.edu>
2020-03-05 06:34:55 -0800

    fixed grading, tried to remove lessons with no grading scripts

--- client/src/dbSource.sql
+++ client/src/dbSource.sql
@@ -26,39 +26,39 @@ VALUES(2,
 
 INSERT INTO Lesson(lesson_id,question, answer, name, hint)
 VALUES(3,
-        'Create a program that will print out the text \"Hello World!\"',
+        'Create a program that will print out the text "Hello World!"',
         'Use the print function',
         'Hello world!',
-        'Use the function print()');
+        'Use the text category');
 
-INSERT INTO Lesson(lesson_id,question, answer, name, hint)
-VALUES(4,
-        'Create an integer, float, char, and string, assign values to them,
-            and print them out with their type',
-        'var a = 10\n
-            var b = 10.05
-            var c = "C"
-            var d = "words" ',
-        'Variables',
-        'Use the type() function');
+-- INSERT INTO Lesson(lesson_id,question, answer, name, hint)
+-- VALUES(4,
+--         'Create an integer, float, char, and string, assign values to them,
+--             and print them out with their type',
+--         'var a = 10\n
+--             var b = 10.05
+--             var c = "C"
+--             var d = "words" ',
+--         'Variables',
+--         'Use the type() function');
 
-INSERT INTO Lesson(lesson_id,question, answer, name, hint)
-VALUES(5,
-        'Declare 3 integers. Add the first two together and save the result.\n
-            Then subtract the third from that result. \n
-            Then multiply that result by the first number.\n
-            Then divide the result by the second number.\n
-            Print the final result.\n',
-        'var a = 2\n
-            var b = 5\n
-            var c = 8\n
-            var result = a + b\n
-            result = result - c\n
-            result = result * a\n
-            result = result / 2\n
-            print result\n',
-        'Basic Operators',
-        'Remember these operators: (+,-,*,/)');
+-- INSERT INTO Lesson(lesson_id,question, answer, name, hint)
+-- VALUES(5,
+--         'Declare 3 integers. Add the first two together and save the result.\n
+--             Then subtract the third from that result. \n
+--             Then multiply that result by the first number.\n
+--             Then divide the result by the second number.\n
+--             Print the final result.\n',
+--         'var a = 2\n
+--             var b = 5\n
+--             var c = 8\n
+--             var result = a + b\n
+--             result = result - c\n
+--             result = result * a\n
+--             result = result / 2\n
+--             print result\n',
+--         'Basic Operators',
+--         'Remember these operators: (+,-,*,/)');
 
 ---------------------------------------------------------------------
 CREATE TABLE Student(
