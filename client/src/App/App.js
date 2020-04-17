52e7e8def6d77e349975d90d8fb32ff9eadf081a
Rebecca Croysdale <croysdar@oregonstate.edu>
2020-04-16 19:10:11 -0700

    Made a component for each teacher view, not filled in yet

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -20,6 +20,12 @@ import CardGame from '../Menus/CardGameMenu.js'
 //import LessonTemplate from '../Lesson_comps/LessonTemplate.js'
 import '../CSS_files/App.css';
 import RegisterChoice from '../Menus/RegisterChoice.js';
+import TeacherHome from '../Menus/TeacherView/TeacherHome.js';
+import ManageAllStudents from '../Menus/TeacherView/ManageAllStudents.js';
+import ManageStudent from '../Menus/TeacherView/ManageStudent.js';
+import ManageAllLessons from '../Menus/TeacherView/ManageAllLessons.js';
+import ManageLesson from '../Menus/TeacherView/ManageLesson.js';
+import NewLesson from '../Menus/TeacherView/NewLesson.js';
 
 class App extends Component {
   state = {
@@ -77,6 +83,42 @@ class App extends Component {
           {/* <Route exact path="/CardGame" component={CardGame} /> */}
           <ProtectedRoute path='/Lesson/:lessonID' loggedIn={this.state.loggedIn} component={LessonScreen} />
           {/* <Route path='/Lesson/:lessonID' component={LessonScreen} /> */}
+                    {/* The components below should only be accessible for logged in students*/}
+          <ProtectedRoute exact path="/Home" loggedIn={this.state.loggedIn} component={HomeScreen} />
+          {/* <Route exact path="/Home" component={HomeScreen} /> */}
+          <ProtectedRoute exact path="/LessonMenu" loggedIn={this.state.loggedIn} component={LessonMenu} />
+          {/* <Route exact path="/LessonMenu" component={LessonMenu} /> */}
+          <ProtectedRoute exact path="/CardGame" loggedIn={this.state.loggedIn} component={CardGame} />
+          {/* <Route exact path="/CardGame" component={CardGame} /> */}
+          <ProtectedRoute path='/Lesson/:lessonID' loggedIn={this.state.loggedIn} component={LessonScreen} />
+          {/* <Route path='/Lesson/:lessonID' component={LessonScreen} /> */}
+
+          {/* The components below should only be accessible for logged in teachers*/}
+          <ProtectedRoute exact path='/teacherHome' loggedIn={this.state.teacherLoggedIn} component={TeacherHome} />
+
+            {/* /teacherHome/managestudents */}
+              {/* should be a list of all students with their overall grades */}
+              {/* should have */}
+                {/* button for delete student */}
+                {/* student's overall grade */}
+                {/* student's grade for each assignment */}
+          <ProtectedRoute exact path='/teacherHome/manageStudents' loggedIn={this.state.teacherLoggedIn} component={ManageAllStudents} />
+
+            {/* /teacherHome/managestudents/:studentID */}
+          <ProtectedRoute path='/teacherHome/manageStudents/:studentID' loggedIn={this.state.teacherLoggedIn} component={ManageStudent} />
+
+            {/* /teacherHome/managelessons */}
+                {/* teacher can click individual lessons */}
+                {/* teacher can make new lesson */}
+          <ProtectedRoute exact path='/teacherHome/manageLessons' loggedIn={this.state.teacherLoggedIn} component={ManageAllLessons} />
+
+              {/* /teacherHome/managelessons/:lessonID */}
+              {/* should be able to edit all parts of lesson, save, or delete */}
+          <ProtectedRoute path='/teacherHome/manageLessons/:lessonID' loggedIn={this.state.teacherLoggedIn} component={ManageLesson} />
+
+              {/* /teacherHome/managelessons/newLesson */}
+          <ProtectedRoute exact path='/teacherHome/manageLessons/newLesson' loggedIn={this.state.teacherLoggedIn} component={NewLesson} />
+
 
         </Router>
 
