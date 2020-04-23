f651541be786ee46e22ab60f95f9ced2b7506e89
Rebecca Croysdale <croysdar@oregonstate.edu>
2020-04-22 17:05:53 -0700

    Adding New Lesson api call

--- client/src/Screens/TeacherView/ManageLesson.js
+++ client/src/Screens/TeacherView/ManageLesson.js
@@ -13,50 +13,86 @@ class ManageLesson extends Component {
     hint:''
   }
 
-render() {
+  handleSave = async e => {
+    e.preventDefault();
+    var name = this.state.name;
+    var question = this.state.question;
+    var hint = this.state.hint;
+    // check that none are empty
+    if ((name == '') || (question = '') || (hint = '')){
+      alert("Must fill in all fields")
+      return;
+    }
+
+
+    const response = await fetch('/NewLesson', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "name": name,
+        "question": question,
+        "hint": hint,
+      })
+    });
+    const body = await response.text();
+    console.info("Created " + body);
+    alert("Lesson Created");
+    // alert("Lesson Created" + body.data.name);
+
+    // if (body == 'Failure') {
+    //   alert("Username taken");
+    //   console.info("Taken: " + body);
+    // } else if (body == "DB Failure") {
+    //   alert("Issue creating account");
+    // } else {
+    //   console.info("Created " + body);
+    //   alert("User created");
+    // }
+  };
+
+  render() {
     return (
       <div className="Edit-Lesson">
-        <ButtonGroup large style={{paddingBottom: "1vh"}}>
+        <ButtonGroup large style={{ paddingBottom: "1vh" }}>
           <Link to="/ManageLessons">
             <Button
+              text="Back"
               className={"Top-Left-Back"}
-              style={{marginRight: "1vh"}}
-              type="button"
+              style={{ marginRight: "1vh" }}
               intent="warning"
               icon="small-cross"
-              text="Back"
             />
           </Link>
-          <Link to="/">
-            <Button
-              type="button"
-              intent="success"
-              icon="small-cross"
-              text="Save"
-            />
-          </Link>
+          <Button
+            text="Save"
+            intent="success"
+            icon="small-cross"
+            onClick={(e) => this.handleSave(e)}
+          />
         </ButtonGroup>
         <EditField
           title="Lesson Name"
           placeholder="Click to edit..."
           value={this.state.name}
-          onChange={(e) => this.setState({name: e.value})}
+          onChange={(e) => this.setState({ name: e.value })}
         />
-        <br/>
+        <br />
         <EditField
           title="Question"
           placeholder="Click to edit..."
           value={this.state.question}
-          onChange={(e) => this.setState({question: e.value})}
+          onChange={(e) => this.setState({ question: e.value })}
         />
-        <br/>
+        <br />
         <EditField
           title="Hint"
           placeholder="Click to edit..."
           value={this.state.hint}
-          onChange={(e) => this.setState({hint: e.value})}
+          onChange={(e) => this.setState({ hint: e.value })}
         />
-        <br/>
+        <br />
         <BlocklyCompEdit lessonID={this.props.match.params.lessonID} />
       </div>
     );
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const rimraf = require('rimraf')
 const bcrypt  = require('bcrypt');
 const app = express();
 const port = process.env.PORT || 5000;
+
 //Open and load database into object
 let db = new sqlite.Database('./client/src/database.db', (err) =>{
     if (err){
@@ -20,25 +21,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const { exec } = require("child_process");
-function runCmd(cmd,callback) {
-   exec(cmd, (error, stdout, stderr) => {
-      if (error != null) {
-         console.log(`error: ${error.message}`);
-         if (callback) {
-            return callback("","Something went wrong.");
-         }
-      }
-      if (stderr) {
-         if(callback) {
-            return callback("\`\`\`"+stderr+"\n"+stdout+"\`\`\`");
-         }
-      }
-      //console.log(stdout);
-      if (callback) {
-          //console.log("Callback stdout");
-      return callback(stdout);
-      }
-   });
+function runCmd(cmd, callback) {
+    exec(cmd, (error, stdout, stderr) => {
+        if (error != null) {
+            console.log(`error: ${error.message}`);
+            if (callback) {
+                return callback("", "Something went wrong.");
+            }
+        }
+        if (stderr) {
+            if (callback) {
+                return callback("\`\`\`" + stderr + "\n" + stdout + "\`\`\`");
+            }
+        }
+        //console.log(stdout);
+        if (callback) {
+            //console.log("Callback stdout");
+            return callback(stdout);
+        }
+    });
 }
 //Register a user. If username already exists, return "Failure"
 app.post('/api/register', (req, res) => {
@@ -102,7 +103,7 @@ app.post('/api/login', (req, res) => {
 //---------------------------------------------------------------  Grading api calls below here ----------------------------------------------------
 
 app.get('/api/connect', (req, res) => {
-  res.send({ express: 'Connected to the grading server' });
+    res.send({ express: 'Connected to the grading server' });
 });
 
 app.post('/api/grade', (req, res) => {
@@ -111,13 +112,13 @@ app.post('/api/grade', (req, res) => {
     var response;
     //var rand = Math.floor((Math.random() * 10000) + 1);
 //right now it is hard coded for saving to user id 6969. this can be changed
-    runCmd("printf \""+req.body.code+"\" > ./users/6969/pcode/"+req.body.lesson+" && ./backend/run_python_script.sh ./grading_scripts/"+req.body.lesson+" ./users/6969/pcode/"+req.body.lesson+" "+"6969" +" && rm ./users/6969/pcode/"+req.body.lesson,function(text,error) {
-  console.log(text);
+    runCmd("printf \"" + req.body.code + "\" > ./users/6969/pcode/" + req.body.lesson + " && ./backend/run_python_script.sh ./grading_scripts/" + req.body.lesson + " ./users/6969/pcode/" + req.body.lesson + " " + "6969" + " && rm ./users/6969/pcode/" + req.body.lesson, function (text, error) {
+        console.log(text);
 
-  res.send(
-   `I received your POST request. This is what you sent me: ` + text,
- );
-});
+        res.send(
+            `I received your POST request. This is what you sent me: ` + text,
+        );
+    });
 
 });
 
@@ -166,9 +167,12 @@ module.exports = (app, db) =>{
         });
     });
 
-    app.post('/NewLesson/', (req, res) => {
+    // Make a new lesson
+    app.post('/NewLesson', (req, res) => {
         // let sql = 'SELECT * FROM Lesson WHERE lesson_id = ?';
-        let sql = 'INSERT INTO Lesson VALUES ?';
+        let sql = 'INSERT INTO Lesson(lesson_id, question, answer, name, hint) VALUES (?,?,?,?,?)';
+        // TODO add lesson number
+        // TODO add group by lesson_number ascending
 
         let lessonID = 100;
         let lessonName = [req.params.name];
@@ -176,8 +180,6 @@ module.exports = (app, db) =>{
         let lessonHint = [req.params.hint];
 
         let values = [lessonID, lessonName, lessonQuestion, lessonHint]
-
-        
 
         //get query to database for lesson with :id
         db.get(sql, values, (err, row) => {
