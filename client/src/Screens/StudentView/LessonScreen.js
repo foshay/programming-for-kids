9dc589ce1cf8de0c91b8605fc4f4e021ad2fea88
Rebecca Croysdale <croysdar@oregonstate.edu>
2020-05-03 17:10:29 -0700

    Added loading transition to LessonScreen

--- client/src/Screens/StudentView/LessonScreen.js
+++ client/src/Screens/StudentView/LessonScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import BlocklyComp from '../../Blockly_comps/BlocklyComp.js'
+import BounceLoader from 'react-spinners/BounceLoader';
+import LoadingSymbol from '../../SmallComponents/LoadingSymbol.js';
 
 class LessonScreen extends Component {
     state = {
         question: "[Loading]",
         hint: "[Loading]",
         answer: "[Loading]",
+        isLoading: true,
     }
 
     getLesson = async () => {
@@ -13,6 +16,7 @@ class LessonScreen extends Component {
         console.log("String: " + string);
         return fetch('/api/lesson/' + string)
         .then(response =>{
+            this.setState({isLoading: false});
             return response.json();
         })
         .then(json =>{
@@ -29,16 +33,21 @@ class LessonScreen extends Component {
     }
 
     render(){
-        return(
-            <div className="BodyMenu-Lesson">
-                <h3>Goal: {this.state.question}</h3>
-                <h3>Hint: {this.state.hint}</h3>
-                <BlocklyComp
-                    lessonID={this.props.match.params.lessonID}
-                />
-                <h3>Answer: {this.state.answer}</h3>
-            </div>
-        )
+        if (this.state.isLoading){
+            return (<LoadingSymbol/>);
+        }
+        else {
+            return (
+                <div className="BodyMenu-Lesson">
+                    <h3>Goal: {this.state.question}</h3>
+                    <h3>Hint: {this.state.hint}</h3>
+                    <BlocklyComp
+                        lessonID={this.props.match.params.lessonID}
+                    />
+                    <h3>Answer: {this.state.answer}</h3>
+                </div>
+            )
+        }
     }
 }
 
